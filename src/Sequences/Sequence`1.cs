6e79793c7861d645bdceb6b4fd627bd01484cc06
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-04-15 13:37:30 +0100

    Defined main data members and empty sequence

--- src/Sequences/Sequence`1.cs
+++ src/Sequences/Sequence`1.cs
@@ -6,7 +6,91 @@ using System.Threading.Tasks;
 
 namespace Sequences
 {
+    /// <summary>
+    /// Represents an immutable lazy sequence of elements.
+    /// Elements are only evaluated when they're needed, and <see cref="Sequence{T}"/> employs memoization to store the computed values and avoid re-evaluation.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the sequence.</typeparam>
     public class Sequence<T>
     {
+        private readonly T _head;
+        private readonly Lazy<Sequence<T>> _tail;
+
+        /// <summary>
+        /// Tests whether the sequence is empty.
+        /// </summary>
+        public virtual bool IsEmpty
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Returns the first element of this sequence.
+        /// </summary>
+        public virtual T Head
+        {
+            get { return _head; }
+        }
+
+        /// <summary>
+        /// Returns a sequence of all elements except the first.
+        /// </summary>
+        public virtual Sequence<T> Tail
+        {
+            get { return _tail.Value; }
+        }
+
+        private static readonly Lazy<Sequence<T>> _empty =
+            new Lazy<Sequence<T>>(() => new EmptySequence());
+
+        /// <summary>
+        /// Returns an empty sequence.
+        /// </summary>
+        public static Sequence<T> Empty
+        {
+            get { return _empty.Value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="Sequence{T}"/>.
+        /// </summary>
+        /// <param name="head">The first element of the sequence.</param>
+        /// <param name="tail">A delegate that will be used to realize the sequence's tail when needed.</param>
+        public Sequence(T head, Func<Sequence<T>> tail) : this(head, new Lazy<Sequence<T>>(tail))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="Sequence{T}"/>.
+        /// </summary>
+        /// <param name="head">The first element of the sequence.</param>
+        /// <param name="tail">The tail of the sequence.</param>
+        protected Sequence(T head, Lazy<Sequence<T>> tail)
+        {
+            _head = head;
+            _tail = tail;
+        }
+
+        private class EmptySequence : Sequence<T>
+        {
+            public EmptySequence() : base(default(T), null as Lazy<Sequence<T>>)
+            {
+            }
+
+            public override bool IsEmpty
+            {
+                get { return true; }
+            }
+
+            public override T Head
+            {
+                get { throw new InvalidOperationException(); }
+            }
+
+            public override Sequence<T> Tail
+            {
+                get { throw new InvalidOperationException(); }
+            }
+        }
     }
 }
