cef4287af366f6c06307f7038fbc6e4107c88715
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-04-15 18:46:26 +0100

    Added From factory methods

--- src/Sequences/Sequence.cs
+++ src/Sequences/Sequence.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sequences
 {
+    /// <summary>
+    /// Provides a set of static methods for creating instances of <see cref="Sequence{T}"/>.
+    /// </summary>
     public static class Sequence
     {
+        /// <summary>
+        /// Creates an infinite sequence starting at <paramref name="start"/> and incrementing by <paramref name="step"/> in each step.
+        /// </summary>
+        /// <param name="start">The start value of the sequence.</param>
+        /// <param name="step">The value to increment in each step.</param>
+        /// <returns>A sequence starting at value <paramref name="start"/>.</returns>
+        public static Sequence<int> From(int start, int step)
+        {
+            return new Sequence<int>(start, () => From(start + step, step));
+        }
+
+        /// <summary>
+        /// Creates an infinite sequence starting at <paramref name="start"/> and incrementing by 1 in each step.
+        /// </summary>
+        /// <param name="start">The start value of the sequence.</param>
+        /// <returns>A sequence starting at value <paramref name="start"/>.</returns>
+        public static Sequence<int> From(int start)
+        {
+            return From(start, 1);
+        }
+
+        /// <summary>
+        /// Creates an infinite sequence starting at <paramref name="start"/> and incrementing by <paramref name="step"/> in each step.
+        /// </summary>
+        /// <param name="start">The start value of the sequence.</param>
+        /// <param name="step">The value to increment in each step.</param>
+        /// <returns>A sequence starting at value <paramref name="start"/>.</returns>
+        public static Sequence<long> From(long start, int step)
+        {
+            return new Sequence<long>(start, () => From(start + step, step));
+        }
+
+        /// <summary>
+        /// Creates an infinite sequence starting at <paramref name="start"/> and incrementing by 1 in each step.
+        /// </summary>
+        /// <param name="start">The start value of the sequence.</param>
+        /// <returns>A sequence starting at value <paramref name="start"/>.</returns>
+        public static Sequence<long> From(long start)
+        {
+            return From(start, 1);
+        }
+
+        /// <summary>
+        /// Creates an infinite sequence starting at <paramref name="start"/> and incrementing by <paramref name="step"/> in each step.
+        /// </summary>
+        /// <param name="start">The start value of the sequence.</param>
+        /// <param name="step">The value to increment in each step.</param>
+        /// <returns>A sequence starting at value <paramref name="start"/>.</returns>
+        public static Sequence<BigInteger> From(BigInteger start, int step)
+        {
+            return new Sequence<BigInteger>(start, () => From(start + step, step));
+        }
+
+        /// <summary>
+        /// Creates an infinite sequence starting at <paramref name="start"/> and incrementing by 1 in each step.
+        /// </summary>
+        /// <param name="start">The start value of the sequence.</param>
+        /// <returns>A sequence starting at value <paramref name="start"/>.</returns>
+        public static Sequence<BigInteger> From(BigInteger start)
+        {
+            return From(start, 1);
+        }
     }
 }
---
+++ tests/Sequences.Tests/FactoryMethodsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sequences.Tests
+{
+    public class FactoryMethodsTests
+    {
+        [Fact]
+        public void FromInt_Generates_ConsecutiveIntegers()
+        {
+            int[] expected = {1, 2, 3, 4, 5};
+
+            var intSequence = Sequence.From(1);
+            Assert.Equal(expected, intSequence.Take(5));
+
+            var longSequence = Sequence.From(1L);
+            Assert.Equal(expected.Select(i => (long) i), longSequence.Take(5));
+
+            var bigIntSequence = Sequence.From((BigInteger) 1);
+            Assert.Equal(expected.Select(i => (BigInteger) i), bigIntSequence.Take(5));
+        }
+
+        [Fact]
+        public void FromInt_WithStep_Generates_IncreasingSequence()
+        {
+            int[] expected = {0, 5, 10, 15, 20};
+
+            var intSequence = Sequence.From(0, 5);
+            Assert.Equal(expected, intSequence.Take(5));
+
+            var longSequence = Sequence.From(0L, 5);
+            Assert.Equal(expected.Select(i => (long) i), longSequence.Take(5));
+
+            var bigIntSequence = Sequence.From((BigInteger) 0, 5);
+            Assert.Equal(expected.Select(i => (BigInteger) i), bigIntSequence.Take(5));
+        }
+    }
+}
