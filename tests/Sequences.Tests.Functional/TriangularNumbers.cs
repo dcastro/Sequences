79e43918af357f215c048e014de249fdf116b78f
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-05-06 16:53:26 +0100

    Added Triangular Numbers series

---
+++ tests/Sequences.Tests.Functional/TriangularNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sequences.Tests.Functional
+{
+    public class TriangularNumbers
+    {
+        private readonly ISequence<int> _expectedNumbers = Sequence.With(0, 1, 3, 6, 10, 15, 21, 28, 36, 45);
+
+        [Fact]
+        public void V1()
+        {
+            var numbers = Sequence.From(0)
+                                  .Select(n => n*(n + 1)/2);
+
+            Assert.Equal(_expectedNumbers, numbers.Take(10));
+        }
+
+        [Fact]
+        public void V2()
+        {
+            var numbers = Triangular(0, 0);
+
+            Assert.Equal(_expectedNumbers, numbers.Take(10));
+        }
+
+        private ISequence<int> Triangular(int triangularNumber, int index)
+        {
+            return new Sequence<int>(triangularNumber, () => Triangular(triangularNumber + index + 1, index + 1));
+        } 
+    }
+}
